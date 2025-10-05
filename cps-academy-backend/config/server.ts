e3625124071e2f2b98eef3b200d3a8eda1d078a4
Kutubuddin-Rasel <raselvai257@gmail.com>
2025-10-06 01:46:31 +0600

    enable proxy mode for Railway HTTPS

--- cps-academy-backend/config/server.ts
+++ cps-academy-backend/config/server.ts
@@ -2,6 +2,7 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   url: env('PUBLIC_URL', 'https://cpsacademy-production.up.railway.app'),
+  proxy: true,
   app: {
     keys: env.array('APP_KEYS', [
       'emUoNGkpBV16ukuY1AHXMA==',
