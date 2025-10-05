fda20e7959074fa102f7b1bd0f9cace3a66e171d
Kutubuddin-Rasel <raselvai257@gmail.com>
2025-10-05 23:59:29 +0600

    fix: add PUBLIC_URL to enable admin registration over HTTPS

--- cps-academy-backend/config/server.ts
+++ cps-academy-backend/config/server.ts
@@ -1,6 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'https://cpsacademy-production.up.railway.app'),
   app: {
     keys: env.array('APP_KEYS', [
       'emUoNGkpBV16ukuY1AHXMA==',
