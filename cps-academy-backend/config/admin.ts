53aa5ecbfa928ecf3beb4e3f9bab71b7d0326daf
Kutubuddin-Rasel <raselvai257@gmail.com>
2025-10-06 16:39:04 +0600

    disable secure cookies for Railway deployment

--- cps-academy-backend/config/admin.ts
+++ cps-academy-backend/config/admin.ts
@@ -1,6 +1,11 @@
 export default ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
+    options: {
+      expiresIn: '7d',
+      sameSite: 'lax',
+      secure: false, 
+    },
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
@@ -14,7 +19,7 @@ export default ({ env }) => ({
     encryptionKey: env('ENCRYPTION_KEY'),
   },
   flags: {
-    nps: env.bool('FLAG_NPS', true),
-    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
+    nps: env.bool('FLAG_NPS', false),
+    promoteEE: env.bool('FLAG_PROMOTE_EE', false), 
   },
 });
